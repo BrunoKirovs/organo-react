330ad14d35b8ae1ab2bb8f035a2d0122feee84dc
Bruno Juan <bruno.kirovs@gmail.com>
2025-03-10 23:13:52 -0300

    Update 1

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,7 +1,11 @@
+import './Banner.css'
+
 function Banner() {
     //JSX
     return (
-        <img src="/imagens/_banner.png" alt=""/>
+        <header className="banner">
+        <img src="/imagens/Banner.png" alt=""/>
+        </header>
     )
 
 }
