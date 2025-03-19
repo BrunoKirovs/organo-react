01df8144061f8aa694a03646f751285b905130be
Bruno Juan <bruno.kirovs@gmail.com>
2025-03-18 23:24:44 -0300

    Update Formulario-organo.v1

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -6,16 +6,7 @@ import {useState} from 'react'
 
 const Formulario = (props) => {
 
-    const times = [
-            'Programação',
-            'Front-End',
-            'Data Science',
-            'Devops',
-            'UX e Design',
-            'Mobile',
-            'Inovação e Gestão',
-    ]
-
+    
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -29,6 +20,10 @@ const Formulario = (props) => {
             imagem,
             time,
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
@@ -59,7 +54,7 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
